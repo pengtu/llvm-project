dcec1df40e0aee830a156e2977681c1639f2639d
Tiotto, Ettore <ettore.tiotto@intel.com>
2023-09-12 10:48:11 -0700

    [GENX][JointMatrix]: Add MatrixMad to the GENX dialect
    Signed-off-by: Tiotto, Ettore <ettore.tiotto@intel.com>

--- mlir/lib/Dialect/LLVMIR/IR/GENXOps.cpp
+++ mlir/lib/Dialect/LLVMIR/IR/GENXOps.cpp
@@ -11,11 +11,15 @@
 //===----------------------------------------------------------------------===//
 
 #include "mlir/Dialect/LLVMIR/GENXDialect.h"
+<<<<<<< HEAD
 #include "mlir/Dialect/LLVMIR/GENXTypes.h"
+=======
+>>>>>>> 5e9b906fb9be ([GENX][JointMatrix]: Add MatrixMad to the GENX dialect)
 
 using namespace mlir;
 
 //===----------------------------------------------------------------------===//
+<<<<<<< HEAD
 // genx.matrix.load
 //===----------------------------------------------------------------------===//
 
@@ -44,5 +48,29 @@ LogicalResult GENX::MatrixInitOp::verify() {
   if (matType.getElementType() != getVal().getType())
     return this->emitOpError("initializer type must match matrix element type");
 
+=======
+// genx.matrix.mad
+//===----------------------------------------------------------------------===//
+
+LogicalResult GENX::MatrixMadOp::verify() {
+  if (this->getC().getType() != this->getResult().getType())
+    return this->emitOpError(
+        "result and third operand must have the same type");
+  auto typeA = this->getA().getType().cast<GENX::JointMatrixType>();
+  auto typeB = this->getB().getType().cast<GENX::JointMatrixType>();
+  auto typeC = this->getC().getType().cast<GENX::JointMatrixType>();
+  auto typeR = this->getResult().getType().cast<GENX::JointMatrixType>();
+  if (typeA.getNumRows() != typeR.getNumRows() ||
+      typeA.getNumColumns() != typeB.getNumRows() ||
+      typeB.getNumColumns() != typeR.getNumColumns())
+    return this->emitOpError("matrix size must match");
+  if (typeR.getScope() != typeA.getScope() ||
+      typeR.getScope() != typeB.getScope() ||
+      typeR.getScope() != typeC.getScope())
+    return this->emitOpError("matrix scope must match");
+  if (typeA.getElementType() != typeB.getElementType() ||
+      typeR.getElementType() != typeC.getElementType())
+    return this->emitOpError("matrix element type must match");
+>>>>>>> 5e9b906fb9be ([GENX][JointMatrix]: Add MatrixMad to the GENX dialect)
   return success();
 }
